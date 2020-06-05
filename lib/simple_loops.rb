07fc5981c8930f9ac0cf8d2288288e7bffef7e1f
Sonya Gould <sonyavgould@gmail.com>
2020-06-05 22:52:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,17 @@
 # Write your methods here
 
-def loop_message_five_times 
+def loop_message_five_times(string) 
+  5.times do 
+    print string
+  end
 end
   
-def loop_message_n_times
+def loop_message_n_times(message, integer)
+  times_printed = 0
+  while times_printed <= integer 
+    print string
+    times_printed += 1
+  end
 end
   
 def output_array
